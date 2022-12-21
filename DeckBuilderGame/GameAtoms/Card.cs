4751dfaf16cc149fd41aee4e7968c4e4eca07e53
Jeremiah Dost <jeremy.dost@gmail.com>
2022-12-20 17:09:46 -0800

    Fixes bug in import of cardtype parameter from .xml config
    Card logic import now wrapped in own method and set via loop rather than linq expression to handle higher complexity of importing different types correctly as needed

--- DeckBuilderGame/GameAtoms/Card.cs
+++ DeckBuilderGame/GameAtoms/Card.cs
@@ -33,19 +33,8 @@ namespace DeckBuilderGame.GameAtoms
 			Type = card.CardType;
 			Cost = card.Cost;
 			Value = card.Value;
-			Logic = new Dictionary<MethodInfo, object[]>();
-			Console.WriteLine(card.Name);
-			foreach (var action in card.Logic)
-			{
-				var methodInfo = typeof(Actions).GetMethod(action.Name);
-				var parameterDict = action.Parameters.ToDictionary(p => p.Name, p => p.Value);
-				var parameters = methodInfo.GetParameters().Select(p => 
-						parameterDict.ContainsKey(p.Name) 
-						? Convert.ChangeType(parameterDict[p.Name], p.ParameterType)
-						: Convert.ChangeType(null, p.ParameterType))
-					.ToArray();
-				Logic.Add(methodInfo, parameters);
-			}
+
+			SetLogic(card);
 		}
 
 		public void WriteToConsole()
@@ -92,6 +81,47 @@ namespace DeckBuilderGame.GameAtoms
 		public override int GetHashCode()
 		{
 			return HashCode.Combine(Name);
+		}
+
+		void SetLogic(CardSerializable card)
+		{
+			Logic = new Dictionary<MethodInfo, object[]>();
+			foreach (var action in card.Logic)
+			{
+				var methodInfo = typeof(Actions).GetMethod(action.Name);
+				var parameterDict = action.Parameters.ToDictionary(p => p.Name, p => p.Value);
+				var methodInfoParameters = methodInfo.GetParameters();
+				var logicParameters = new object[methodInfoParameters.Length];
+				for (var i = 0; i < methodInfoParameters.Length; i++)
+				{
+					var methodInfoParameter = methodInfoParameters[i];
+
+
+					if (parameterDict.ContainsKey(methodInfoParameter.Name))
+					{
+						// If we've got the parameter in our dictionary from the XML file, then 
+						// load that value in.
+						var parameterDictValue = parameterDict[methodInfoParameter.Name];
+						if (methodInfoParameter.ParameterType == typeof(CardType))
+						{
+							// Handle CardType parameter specially
+							var cardTypeValue = Enum.Parse(typeof(CardType), parameterDictValue);
+							logicParameters[i] = Convert.ChangeType(cardTypeValue, methodInfoParameter.ParameterType);
+						}
+						else
+						{
+							logicParameters[i] = Convert.ChangeType(parameterDictValue, methodInfoParameter.ParameterType);
+						}
+					}
+					else
+					{
+						// Otherwise, let's set the value to null and we'll set it at runtime (This is for player, gameState, etc.)
+						logicParameters[i] = null;
+					}
+				}
+
+				Logic.Add(methodInfo, logicParameters);
+			}
 		}
 	}
 
